67ac74e059b568cca5f582aeca12bfca4d7c5ae3
qatestuser1 <35654053+qatestuser1@users.noreply.github.com>
2018-08-26 22:10:11 +0300

    test

--- Automation/Functions.py
+++ Automation/Functions.py
@@ -1,3 +1,20 @@
 #random name
 #random email
 #random button?
+import random
+import string
+
+domains = ["hotmail.com", "gmail.com", "aol.com", "mail.com", "mail.kz", "yahoo.com"]
+letters = string.ascii_lowercase[:12]
+
+
+def get_random_domain(domains):
+    return random.choice(domains)
+
+
+def get_random_name(length):
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def generate_random_email(length):
+    return get_random_name(letters, length) + '@' + get_random_domain(domains)
--- Automation/Tests/LogIn.py
+++ Automation/Tests/LogIn.py
@@ -1,6 +1,8 @@
 import Pages
 from EnvironmentSetUp import *
 import unittest
+import Functions
+import time
 
 class LogIn(EnvironmentSettingUp):
     def test_success_log_in(self):
@@ -24,8 +26,9 @@ class LogIn(EnvironmentSettingUp):
         home_page.get_my_account().click()
 
         account = Pages.AccountPage(self.driver)
-        account.get_username_or_email_address().send_keys('sdsad')
-        account.get_password().send_keys('asdasd')
+        account.get_username_or_email_address().send_keys(Functions.get_random_name(12))
+        time.sleep(4)
+        account.get_password().send_keys(Functions.get_random_name(12))
         account.get_log_in().click()
 
         account_page_error = Pages.AccountPageError(self.driver)
